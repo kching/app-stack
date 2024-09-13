c26c75be5a091597d1c634a70a9c5f21416a499c
Kai <kai.y.ching@gmail.com>
2024-09-13 20:57:30 +1000

    added login button to landing page.

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,29 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Hello world, this is a landing page.</div>;
+  return (
+    <div className="mx-2 my-4">
+      <div className="flex justify-between">
+        <h1 className="text-lg ml-4">Sample App</h1>
+        <div>
+          <Button asChild>
+            <Link href="/main">Login</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="m-4">
+        <p>Hello world! This is a public landing page to a sample application.</p>
+        <p>
+          Click
+          <Button asChild variant="link" className="ml-1 p-0">
+            <Link href="/main"> here</Link>
+          </Button>{' '}
+          to proceed.
+        </p>
+      </div>
+    </div>
+  );
 }
