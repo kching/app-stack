e8a8001402532f302e8aa4b5682cd37d5694bb47
Kai <kai.y.ching@gmail.com>
2024-09-13 20:30:12 +1000

    light mode/dark mode

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body className={`h-full w-full overflow-hidden ${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <main className="h-full">{children}</main>
           <Toaster />
         </ThemeProvider>
--- packages/web/src/components/workspace/HeaderBar.tsx
+++ packages/web/src/components/workspace/HeaderBar.tsx
@@ -4,19 +4,30 @@ import React, { useMemo } from 'react';
 import { signOut } from 'next-auth/react';
 import { usePageContext } from '@/components/workspace/PageStateProvider';
 import { Button } from '@/components/ui/button';
-import { PowerIcon, SettingsIcon } from 'lucide-react';
+import { PowerIcon, MoonIcon, SunIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useTheme } from 'next-themes';
 
 const HeaderBar = () => {
   const router = useRouter();
+  const theme = useTheme();
+  const { resolvedTheme, setTheme } = theme;
   const { state } = usePageContext();
   const title = useMemo(() => state.title, [state]);
   return (
     <div className="m-2 ml-0 flex justify-between items-center flex-grow">
       <h2 className="text-lg font-bold">{title}</h2>
       <span>
-        <Button variant="ghost" className="px-2">
-          <SettingsIcon className="h-5 w-5" />
+        <Button
+          variant="ghost"
+          className="px-2"
+          onClick={() => {
+            setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+          }}
+        >
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
         </Button>
         <Button
           variant="ghost"
